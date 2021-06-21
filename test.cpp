954f8df0e3335991e0c7d85b74804a56565a8a93
htagrour <hamza08tagrourete@gmail.com>
2021-06-21 16:31:21 +0100

    hello

--- test.cpp
+++ test.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <list>
+#include <memory>
 
-template <typename T>
-class Test
-{
-    typedef  hi;
-    public:
-        hi var;
-};
 
 int main(void)
-{ 
+{
+    std::vector<int> vet;
+
+    for (int i = 0; i < 10; i++)
+        vet.
+    
     return (0);
 }
+// https://www.youtube.com/watch?v=F9eDv-YIOQ0
+// https://internalpointers.com/post/writing-custom-iterators-modern-cpp
+// http://www2.lawrence.edu/fast/GREGGJ/CMSC270/linked/iterators.html
